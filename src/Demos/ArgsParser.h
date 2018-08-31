75f8597b410c19fd9b056747685117e71cbf258c
IvanPleshkov <pleshkov.ivan@gmail.com>
2018-09-01 00:40:26 +0300

    fix gcc build

--- src/Demos/ArgsParser.h
+++ src/Demos/ArgsParser.h
@@ -8,6 +8,7 @@ namespace RED_LILIUM_NAMESPACE
 class ArgsParser : public RedLiliumObject
 {
 public:
+
 	ArgsParser(int argc, char** argv);
 	~ArgsParser() override {}
 
@@ -16,52 +17,62 @@ public:
 	template<typename T>
 	T Get(const std::string& key, int index = 0) const;
 
-	template<>
-	std::string Get<std::string>(const std::string& key, int index) const;
-
-	template<>
-	int Get<int>(const std::string& key, int index) const;
-
-	template<>
-	double Get<double>(const std::string& key, int index) const;
-
 	template<typename T>
 	T GetDefault(const std::string& key, T default_value) const;
 
 private:
-	std::unordered_map<std::string, std::vector<std::string>> m_options;
+    using OptionsMap = std::unordered_map<std::string, std::vector<std::string>>;
 
+    OptionsMap m_options;
 	std::vector<std::string> m_parameters;
 };
+
+namespace ArgsParserDetails
+{
+
+template<typename T>
+T Get(const std::string& parameter)
+{
+    throw std::exception();
+}
+
+/*
+template<>
+std::string Get<std::string>(const std::string& parameter)
+{
+    return parameter;
+}
+
+template<>
+int Get<int>(const std::string& parameter)
+{
+    return std::stoi(parameter);
+}
+
+template<>
+double Get<double>(const std::string& parameter)
+{
+    return std::stod(parameter);
+}
+*/
+}
 
 template<typename T>
 T ArgsParser::GetDefault(const std::string& key, T default_value) const
 {
-	if (!Has(key, 0))
-		return default_value;
+    if (!Has(key, 0))
+        return default_value;
 
-	return Get<T>(key);
+    return Get<T>(key);
 }
 
-template<>
-std::string ArgsParser::Get<std::string>(const std::string& key, int index) const
+template<typename T>
+T ArgsParser::Get(const std::string& key, int index) const
 {
-	const auto& option_params = m_options.at(key);
-	return option_params[index];
-}
-
-template<>
-int ArgsParser::Get<int>(const std::string& key, int index) const
-{
-	const auto& option_params = m_options.at(key);
-	return std::stoi(option_params[index]);
-}
-
-template<>
-double ArgsParser::Get<double>(const std::string& key, int index) const
-{
-	const auto& option_params = m_options.at(key);
-	return std::stod(option_params[index]);
+    RED_LILIUM_ASSERT(m_options.count(key) > 0);
+    const auto& optionParams = m_options.at(key);
+    RED_LILIUM_ASSERT(optionParams.size() > index);
+    return ArgsParserDetails::Get<T>(optionParams[index]);
 }
 
 } // namespace RED_LILIUM_NAMESPACE
