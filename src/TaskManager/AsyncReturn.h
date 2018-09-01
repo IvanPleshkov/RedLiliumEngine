a398766eccf92271b35c461da11453be106d1446
Pleshkov <pleshkov.ivan@gmail.com>
2018-09-01 20:59:32 +0300

    Task Manager: lite future

---
+++ src/TaskManager/AsyncReturn.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "TaskManagerForward.h"
+
+namespace RED_LILIUM_NAMESPACE
+{
+
+template<class T>
+class AsyncResult
+{
+public:
+	AsyncResult(const T& waitingValue);
+	const T& Get() const;
+	bool IsReady() const;
+
+private:
+	template<class T>
+	friend class AsyncReturn;
+
+	using SharedResult = std::pair<T, std::atomic<bool>>;
+	sptr<SharedResult> m_result;
+};
+
+template<class T>
+class AsyncReturn
+{
+public:
+	AsyncReturn(const T& waitingValue);
+	AsyncReturn(const AsyncReturn&) = delete; // non construction-copyable
+	AsyncReturn& operator=(const AsyncReturn&) = delete; // non copyable
+
+	const AsyncResult<T>& GetResult();
+	void SetReturn(const T& value);
+	void SetReturn(T&& value);
+
+private:
+	AsyncResult<T> m_result;
+};
+
+template<class T>
+inline AsyncResult<T>::AsyncResult(const T& waitingValue)
+{
+	m_result = smake<SharedResult>(waitingValue, false);
+}
+
+template<class T>
+inline const T& AsyncResult<T>::Get() const
+{
+	RED_LILIUM_ASSERT(IsReady());
+	return m_result->first;
+}
+
+template<class T>
+inline bool AsyncResult<T>::IsReady() const
+{
+	return m_result->second;
+}
+
+template<class T>
+inline AsyncReturn<T>::AsyncReturn(const T& waitingValue)
+	: m_result(waitingValue)
+{ }
+
+template<class T>
+inline const AsyncResult<T>& AsyncReturn<T>::GetResult()
+{
+	return m_result;
+}
+
+template<class T>
+inline void AsyncReturn<T>::SetReturn(const T& value)
+{
+	RED_LILIUM_ASSERT(m_result->m_result->second == false);
+
+	m_result->m_result->first = value;
+	m_result->m_result->second = true;
+}
+
+template<class T>
+inline void AsyncReturn<T>::SetReturn(T&& value)
+{
+	RED_LILIUM_ASSERT(m_result->m_result->second == false);
+
+	m_result->m_result->first = std::move(value);
+	m_result->m_result->second = true;
+}
+
+} // namespace RED_LILIUM_NAMESPACE
